9ab70215c98adaabbc6121c330d63f3e7ce25a85
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-11-02 20:59:16 +0000

    Can get the data pulled correctly

--- src/read.py
+++ src/read.py
@@ -8,6 +8,19 @@ def select_all():
     for record in list_of_heros:
         print(record[1])   
 
+def get_hero_relationship(hero1ID, hero2ID):
+    intial_query = """
+        SELECT relationship_type_id from relationships WHERE hero1_id = %s and hero2_id = %s
+    """
+    
+    type_id = execute_query(intial_query,(hero1ID, hero2ID),).fetchone()
+    type_id = str(type_id)
+    type_id = type_id[1]
+    second_query = """
+        SELECT name from relationship_types where ID = %s"""
+
+    print(execute_query(second_query,(type_id,)).fetchone()[0])    
+
 select_all()
 
-print('Test')
+get_hero_relationship(5, 3)
--- src/sql/directSQL.sql
+++ src/sql/directSQL.sql
@@ -1,1 +1,5 @@
-DROP TABLE heros
+SELECT * FROM heroes;
+ SELECT relationship_type_id from relationships WHERE hero1_id = 5 and hero2_id = 3;
+
+ SELECT name from relationship_types where ID = 1
+ 
